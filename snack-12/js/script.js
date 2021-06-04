16db9fd74d0372c06fc0a2b320e18d999e04dca5
emmepi92 <mariapiacoccioli@live.it>
2021-06-04 17:32:15 +0200

    details

--- snack-12/js/script.js
+++ snack-12/js/script.js
@@ -5,21 +5,20 @@ Gatsby vuole generare una falsa lista di 3 invitati.
 
 var names = ['Ottavio', 'Alfredo', 'Davide', 'Luca', 'Mattia', 'Anna', 'Maria', 'Angela'];
 var surnames = ['Rossi', 'Gialli', 'Bianchi', 'Verdi', 'Nero'];
-var guestList = []
+var guestList = [];
 var guest = '';
 
 document.getElementById("list").innerHTML =  '<strong> Gatsby\'s Guest List </strong> <br/>';
+
 var result = '';
 
 // for 3 random Names
-
 while ( guestList.length < 3) {
     guest = names[Math.floor(Math.random()*names.length)] + ' ' + surnames[Math.floor(Math.random()*surnames.length)];
     if (!guestList.includes(guest)) {
         guestList.push(guest)
         result += '<li>' + guest + '</li>';
-
     }
 }
-document.getElementById("result").innerHTML = result
 
+document.getElementById("result").innerHTML = result;
