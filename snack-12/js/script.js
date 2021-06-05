9848c04e7f1ed28ec7e63aad7f3342c8422fba1c
emmepi92 <mariapiacoccioli@live.it>
2021-06-05 22:43:56 +0200

    bonus on 12

--- snack-12/js/script.js
+++ snack-12/js/script.js
@@ -1,12 +1,16 @@
 /*
 Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, 
 Gatsby vuole generare una falsa lista di 3 invitati.
+Bonus: non ripetere ne nome, ne cognome
 */
 
 var names = ['Ottavio', 'Alfredo', 'Davide', 'Luca', 'Mattia', 'Anna', 'Maria', 'Angela'];
 var surnames = ['Rossi', 'Gialli', 'Bianchi', 'Verdi', 'Nero'];
 var guestList = [];
 var guest = '';
+var name = '';
+var surname = '';
+var pushed = [];
 
 document.getElementById("list").innerHTML =  '<strong> Gatsby\'s Guest List </strong> <br/>';
 
@@ -14,9 +18,12 @@ var result = '';
 
 // for 3 random Names
 while ( guestList.length < 3) {
-    guest = names[Math.floor(Math.random()*names.length)] + ' ' + surnames[Math.floor(Math.random()*surnames.length)];
-    if (!guestList.includes(guest)) {
-        guestList.push(guest)
+    name = names[Math.floor(Math.random()*names.length)];
+    surname = surnames[Math.floor(Math.random()*surnames.length)];
+    guest = name + ' ' + surname;
+    if (!guestList.includes(guest) && !pushed.includes(name) && !pushed.includes(surname)) {
+        guestList.push(guest);
+        pushed.push(name, surname);
         result += '<li>' + guest + '</li>';
     }
 }
