85d3d9bca83f05a85ab2b9ec321183f9867c18b4
emmepi92 <mariapiacoccioli@live.it>
2021-06-05 23:23:42 +0200

    dinamic

--- snack-12/js/script.js
+++ snack-12/js/script.js
@@ -5,21 +5,32 @@ Bonus: non ripetere ne nome, ne cognome
 */
 
 var names = ['Ottavio', 'Alfredo', 'Davide', 'Luca', 'Mattia', 'Anna', 'Maria', 'Angela'];
-var surnames = ['Rossi', 'Gialli', 'Bianchi', 'Verdi', 'Nero'];
+var surnames = ['Rossi', 'Gialli', 'Bianchi', 'Verdi', 'Nero', 'Coccioli'];
 var guestList = [];
 var guest = '';
 var name = '';
 var surname = '';
 var pushed = [];
-var maxGuests = parseInt(prompt('Quanti invitati fake desideri generare? (max 5)'));
+var max = 0;
 
-while (isNaN(maxGuests) || maxGuests > 5 || maxGuests <= 0){
+// set the maximum number of names to generate, whitout doubles
+if (names.length <= surnames.length) {
+    max = names.length;
+} else {
+    max= surnames.length;
+}
+
+// ask how many guests the user wants to generate 
+var maxGuests = parseInt(prompt('Quanti invitati fake desideri generare? (max ' + max + ')'));
+
+// check conditions, dont exceed the max and dont accept 0 
+while (isNaN(maxGuests) || maxGuests > max || maxGuests <= 0){
     if (maxGuests === 0) {
         alert('Lista vuota non accettata');
-        maxGuests = parseInt(prompt('Quanti invitati fake desideri generare? (max 5)'));
+        maxGuests = parseInt(prompt('Quanti invitati fake desideri generare? (max ' + max + ')'));
     } else {
-        alert('Inserisci un numero compreso fra 1 e 5');
-        maxGuests = parseInt(prompt('Quanti invitati fake desideri generare? (max 5)'));
+        alert('Inserisci un numero compreso fra 1 e ' + max);
+        maxGuests = parseInt(prompt('Quanti invitati fake desideri generare? (max ' + max + ')'));
     }
 }
 
@@ -27,7 +38,7 @@ document.getElementById("list").innerHTML =  '<strong> Gatsby\'s Guest List </st
 
 var result = '';
 
-// for maxGuests random Names
+// for random Names
 while ( guestList.length < maxGuests) {
     name = names[Math.floor(Math.random()*names.length)];
     surname = surnames[Math.floor(Math.random()*surnames.length)];
