98924cc29bccad998ab44f2b04634fbae8e60e0a
emmepi92 <mariapiacoccioli@live.it>
2021-06-05 16:42:21 +0200

    details

--- snack-11/js/script.js
+++ snack-11/js/script.js
@@ -1,13 +1,19 @@
 // Inserisci un numero, se è pari stampa il numero,se è dispari stampa il numero successivo
 
+var outputHtml = document.getElementById("result");
 var userNum = parseInt(prompt("inserisci un numero"));
 
-var result;
 
+while (isNaN(userNum)) {
+    alert('Devi inserire un numero');
+    userNum = parseInt(prompt("inserisci un numero"));
+}
+
+var result;
 if ( userNum % 2 === 0 ) {
     result = userNum;
 } else {
     result = userNum + 1;
 }
 
-document.getElementById("result").innerHTML = result;
+outputHtml.innerHTML ='Il numero inserito è ' + userNum + ', con l\' arrotondamento ad un numero pari è ' + result;
